bbb947d71a11e3b4faec1182f7ae23c8a9021f7d
Kseniya Stelmak <stelmak.k.d@gmail.com>
2022-08-23 16:12:46 +0300

    add task 3

---
+++ DataContracts/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataContracts
+{
+    public class BaseEntity
+    {
+        public Guid Id;
+        public string Comment;
+
+        public BaseEntity(Guid id, string comment)
+        {
+            Id = id;
+            Comment = comment;
+        }
+    }
+}
---
+++ DataContracts/Project.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataContracts
+{
+    public class Project : BaseEntity
+    {
+        public string Name;
+        public DateTime ExpirationDate;
+        public int MaxHours;
+        public Guid LeaderUserId;
+
+        public Project(Guid id, string comment, string name, DateTime expirationDate, int maxHours, Guid leaderUserId) : base(id, comment)
+        {
+            Name = name;
+            ExpirationDate = expirationDate;
+            MaxHours = maxHours;
+            LeaderUserId = leaderUserId;
+        }
+    }
+}
